9d3899fb72c37de5a9982007270b81f52666b9ff
ThreeDX <threed@rsgc.ru>
2017-03-01 21:48:30 +0300

    DZ5 fix

--- dz5/clock.js
+++ dz5/clock.js
@@ -1,3 +1,103 @@
-/**
- * Created by Дмитрий on 01.03.2017.
- */
+;(function() {
+    'use strict';
+
+    function Clock(options) {
+        var elem = document.getElementById(options.elem);
+        var clock = document.createElement('div');
+        var button = document.createElement('button');
+        var isStarted = false;
+
+        var alertTimer = null;
+        var clockTimer = null;
+        var alertCounter = 0;
+
+        this.start = start;
+        this.stop = stop;
+
+        init();
+
+        function init() {
+            // Очищаем элемент
+            elem.innerHTML = '';
+            elem.onmousedown = function() {
+                return false;
+            };
+
+            clock.classList.add('clock');
+            clock.innerHTML = getTime();
+
+            button.classList.add('button');
+            button.innerHTML = 'ALERT';
+
+            button.onclick = function() {
+                button.disabled = true;
+                alertCounter = 0;
+                clock.classList.add('red');
+
+                if (isStarted) {
+                    _stop()
+                }
+
+                alertTimer = setInterval(function() {
+                    alertCounter++;
+                    if (alertCounter == 30) {
+                        clearInterval(alertTimer);
+                        alertTimer = null;
+                        clock.classList.remove('red');
+                        button.disabled = false;
+
+                        if (isStarted) {
+                            _start()
+                        }
+                    } else {
+                        clock.classList.toggle('red');
+                    }
+                }, 250);
+            };
+
+            elem.appendChild(clock);
+            elem.appendChild(button);
+        }
+
+        function _start() {
+            if (clockTimer) {
+                return;
+            }
+
+            clock.innerHTML = getTime();
+
+            clockTimer = setInterval(function() {
+                clock.innerHTML = getTime();
+            }, 1000);
+        }
+
+        function _stop() {
+            if (clockTimer) {
+                clearInterval(clockTimer);
+                clockTimer = null;
+            }
+        }
+
+        function stop(pause) {
+            _stop();
+            isStarted = false;
+        }
+
+        function start() {
+            _start();
+            isStarted = true;
+        }
+
+        function getTime() {
+            var d = new Date();
+            return pad(d.getHours()) + ':' + pad(d.getMinutes()) + ':' + pad(d.getSeconds());
+        }
+
+        function pad(str) {
+            return ('0' + str).substr(-2);
+        }
+
+    }
+
+    window.Clock = Clock;
+})();
--- dz5/voter.js
+++ dz5/voter.js
@@ -1,3 +1,37 @@
-/**
- * Created by Дмитрий on 01.03.2017.
- */
+;(function() {
+    'use strict';
+
+    function Voter(options) {
+        var elem = document.getElementById(options.elem);
+        var voteElem = elem.getElementsByClassName('vote')[0];
+        var votes = voteElem.textContent;
+
+        elem.onmousedown = function() {
+            return false;
+        };
+
+        elem.onclick = function(e) {
+            if (e.target.classList.contains('down')) {
+                setVote(votes - 1);
+            }
+
+            if (e.target.classList.contains('up')) {
+                setVote(votes + 1);
+            }
+        };
+
+        this.setVote = setVote;
+
+        function setVote(vote) {
+            if (vote < 0) {
+                vote = 0;
+            }
+
+            votes = vote;
+            voteElem.textContent = votes;
+        }
+
+    }
+
+    window.Voter = Voter;
+})();
