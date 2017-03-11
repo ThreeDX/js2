b955776e0a962f3729f0894b478ba515c62995f2
ThreeDX <threed@rsgc.ru>
2017-03-11 23:20:27 +0300

    DZ7_js1

---
+++ dz7_js1/fig_data.js
@@ -0,0 +1,162 @@
+var fig_data = [
+  {
+    cell: "A2",
+    figure: "Figure",
+    color: "white"
+  },
+  {
+    cell: "B2",
+    figure: "Figure",
+    color: "white"
+  },
+  {
+    cell: "C2",
+    figure: "Figure",
+    color: "white"
+  },
+  {
+    cell: "D2",
+    figure: "Figure",
+    color: "white"
+  },
+  {
+    cell: "E2",
+    figure: "Figure",
+    color: "white"
+  },
+  {
+    cell: "F2",
+    figure: "Figure",
+    color: "white"
+  },
+  {
+    cell: "G2",
+    figure: "Figure",
+    color: "white"
+  },
+  {
+    cell: "H2",
+    figure: "Figure",
+    color: "white"
+  },
+  {
+    cell: "A7",
+    figure: "Figure",
+    color: "black"
+  },
+  {
+    cell: "B7",
+    figure: "Figure",
+    color: "black"
+  },
+  {
+    cell: "C7",
+    figure: "Figure",
+    color: "black"
+  },
+  {
+    cell: "D7",
+    figure: "Figure",
+    color: "black"
+  },
+  {
+    cell: "E7",
+    figure: "Figure",
+    color: "black"
+  },
+  {
+    cell: "F7",
+    figure: "Figure",
+    color: "black"
+  },
+  {
+    cell: "G7",
+    figure: "Figure",
+    color: "black"
+  },
+  {
+    cell: "H7",
+    figure: "Figure",
+    color: "black"
+  },
+  {
+    cell: "A1",
+    figure: "FigureCastle",
+    color: "white"
+  },
+  {
+    cell: "B1",
+    figure: "FigureKnight",
+    color: "white"
+  },
+  {
+    cell: "C1",
+    figure: "FigureBishop",
+    color: "white"
+  },
+  {
+    cell: "D1",
+    figure: "FigureQueen",
+    color: "white"
+  },
+  {
+    cell: "E1",
+    figure: "FigureKing",
+    color: "white"
+  },
+  {
+    cell: "F1",
+    figure: "FigureBishop",
+    color: "white"
+  },
+  {
+    cell: "G1",
+    figure: "FigureKnight",
+    color: "white"
+  },
+  {
+    cell: "H1",
+    figure: "FigureCastle",
+    color: "white"
+  },
+  {
+    cell: "A8",
+    figure: "FigureCastle",
+    color: "black"
+  },
+  {
+    cell: "B8",
+    figure: "FigureKnight",
+    color: "black"
+  },
+  {
+    cell: "C8",
+    figure: "FigureBishop",
+    color: "black"
+  },
+  {
+    cell: "D8",
+    figure: "FigureQueen",
+    color: "black"
+  },
+  {
+    cell: "E8",
+    figure: "FigureKing",
+    color: "black"
+  },
+  {
+    cell: "F8",
+    figure: "FigureBishop",
+    color: "black"
+  },
+  {
+    cell: "G8",
+    figure: "FigureKnight",
+    color: "black"
+  },
+  {
+    cell: "H8",
+    figure: "FigureCastle",
+    color: "black"
+  }
+];
